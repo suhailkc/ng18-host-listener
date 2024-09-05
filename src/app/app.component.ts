284653d9ce764e7c45056ddb0a4a4419d5294855
SUHAIL KC <suhailkc@MacBooks-MacBook-Pro.local>
2024-09-05 14:41:27 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,35 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'host-listener';
+
+  showsWebIcon = false;
+  showsPublicIcon = false;
+
+  @ViewChild('five') divFive !: ElementRef;
+  @ViewChild('eight') divEight !: ElementRef;
+
+  private hideAllIcons () {
+    this.showsWebIcon = false;
+    this.showsPublicIcon = false;
+  }
+
+  @HostListener('document:scroll', ['$event'])
+  public onViewportScroll() {
+    const windowHeight = window.innerHeight;
+    const boundingRectFive = this.divFive.nativeElement.getBoundingClientRect();
+    const boundingRectEight = this.divEight.nativeElement.getBoundingClientRect();
+
+    console.log(boundingRectFive);
+    if (boundingRectFive.top >= 0 && boundingRectFive.bottom <= windowHeight) {
+      setTimeout(() => {this.showsWebIcon = true}, 500);
+    } else if (boundingRectEight.top >= 0 && boundingRectEight.bottom <= windowHeight) {
+      setTimeout(() => {this.showsPublicIcon = true}, 500);
+    } else {
+      this.hideAllIcons();
+    }
+
+  }
+
+
+
 }
